7f62827b30984e08891bfbb0111353bc8d9cae5c
Shrimp <lcamara0916@gmail.com>
2022-08-31 20:08:17 +0000

    Made a first implementation to the Guess a Number game

--- C-Sharp/Guess a Number/Program.cs
+++ C-Sharp/Guess a Number/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+
+    static void Main(string[] args)
+    {
+        Game game = new Game();
+        game.Start();
+    }
+
+}
+
+class Game
+{
+
+    public int randomNumber { get; set; }
+    public int tries { get; set; }
+    public bool win { get; set; }
+
+    public void Start()
+    {
+        randomNumber = GenerateRandomNumber();
+        Play();
+    }
+
+    public void Play()
+    {
+        int guess;
+
+        while (!win)
+        {
+            Console.WriteLine("Guess a number: ");
+            Int32.TryParse(Console.ReadLine(), out guess);
+            tries++;
+            CheckNumber(guess);
+        }
+
+    }
+
+    public int GenerateRandomNumber()
+    {
+        return new Random().Next(100);
+    }
+
+    public void CheckNumber(int guess)
+    {
+
+        if (guess == randomNumber)
+        {
+            Console.WriteLine($"Congratulations! You won within {tries} tries");
+            win = true;
+            return;
+        }
+        else if (guess < randomNumber)
+        {
+            Console.WriteLine("Wrong! Too low!");
+            return;
+        }
+
+        Console.Write("Wrong! Too high!");
+    }
+
+}
