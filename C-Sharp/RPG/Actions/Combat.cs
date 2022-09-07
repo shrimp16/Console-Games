2bf8bc1806afa511c6abd806cdf66cb72fc1b75b
Shrimp <lcamara0916@gmail.com>
2022-09-07 22:43:06 +0000

    Moved the combat sample to the combat class

--- C-Sharp/RPG/Actions/Combat.cs
+++ C-Sharp/RPG/Actions/Combat.cs
@@ -1,14 +1,27 @@
 using Enemies;
 
-namespace Combat;
-
 public class Combat
 {
     public IEnemy Enemy { get; private set; }
     public Player Player { get; private set; }
+    public bool inCombat = false;
     public Combat(IEnemy Enemy, Player Player)
     {
         this.Enemy = Enemy;
         this.Player = Player;
+    }
+
+    public void StartCombat(){
+        inCombat = true;
+        while(inCombat){
+            Enemy.TakeDamage(Player.AttackDamage);
+            Player.TakeDamage(Enemy.Damage);
+            Console.WriteLine("Player HP: {0}", Player.Health);
+            Console.WriteLine("Enemy HP: {0}", Enemy.Health);
+
+            if(Enemy.Health <= 0 || Player.Health <= 0){
+                inCombat = false;
+            }
+        }
     }
 }
--- C-Sharp/RPG/Program.cs
+++ C-Sharp/RPG/Program.cs
@@ -7,13 +7,7 @@ class Program
         EnemiesFactory factory = new EnemiesFactory();
         IEnemy enemy = factory.GenerateEnemy();
         Player player = new Player(1000);
-        Console.WriteLine(enemy.ToString());
-        while (player.Health > 0 && enemy.Health > 0)
-        {
-            enemy.TakeDamage(player.AttackDamage);
-            player.TakeDamage(enemy.Damage);
-            Console.WriteLine("Player HP: {0}", player.Health);
-            Console.WriteLine("Enemy HP: {0}", enemy.Health);
-        }
+        Combat combat = new Combat(enemy, player);
+        combat.StartCombat();
     }
 }
