8e05edbb82a029ba774c9f079f20eeab71196a15
Shrimp <lcamara0916@gmail.com>
2022-09-12 23:31:31 +0000

    Made a basic implementation of skills for the mage class

--- C-Sharp/RPG/Game/Player/Classes/Mage.cs
+++ C-Sharp/RPG/Game/Player/Classes/Mage.cs
@@ -6,16 +6,19 @@ class Mage
 {
     public void BasicSkill(Player player, IEnemy enemy)
     {
-
+        Console.WriteLine("Fire ball!");
+        enemy.TakeDamage(player.MagicDamage / 2);
     }
 
     public void MediumSkill(Player player, IEnemy enemy)
     {
-
+        Console.WriteLine("Dragon breath!");
+        enemy.TakeDamage(player.MagicDamage);
     }
 
     public void UltimateSkill(Player player, IEnemy enemy)
     {
-
+        Console.WriteLine("Lightening bolt!");
+        enemy.TakeDamage(player.MagicDamage * 2);
     }
 }
