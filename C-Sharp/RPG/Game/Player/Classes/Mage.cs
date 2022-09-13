8a01fc15410b6c48c9833f4d7c26fd228515f66b
Shrimp <lcamara0916@gmail.com>
2022-09-13 21:44:27 +0000

    Added mana

--- C-Sharp/RPG/Game/Player/Classes/Mage.cs
+++ C-Sharp/RPG/Game/Player/Classes/Mage.cs
@@ -6,18 +6,33 @@ class Mage : IClass
 {
     public void BasicSkill(Player player, IEnemy enemy)
     {
+        if(player.Mana < 50){
+            Console.WriteLine("Not enough mana!");
+            return;
+        }
+        player.Mana -= 50;
         Console.WriteLine("Fire ball!");
         enemy.TakeDamage(player.MagicDamage / 2);
     }
 
     public void MediumSkill(Player player, IEnemy enemy)
     {
+        if(player.Mana < 100){
+            Console.WriteLine("Not enough mana!");
+            return;
+        }
+        player.Mana -= 100;
         Console.WriteLine("Dragon breath!");
         enemy.TakeDamage(player.MagicDamage);
     }
 
     public void UltimateSkill(Player player, IEnemy enemy)
     {
+        if(player.Mana < 500){
+            Console.WriteLine("Not enough mana!");
+            return;
+        }
+        player.Mana -= 500;
         Console.WriteLine("Lightening bolt!");
         enemy.TakeDamage(player.MagicDamage * 2);
     }
--- C-Sharp/RPG/Game/Player/Player.cs
+++ C-Sharp/RPG/Game/Player/Player.cs
@@ -7,19 +7,23 @@ public class Player
     public IClass PlayerClass { get; set; } = null!;
     public int Health { get; set; }
     public int Resistance { get; set; }
+    public int Mana { get; set; }
     public int AttackDamage { get; set; }
     public int MagicDamage { get; set; }
     public IItems[]? Inventory { get; set; }
     public IPets[]? Pets { get; set; }
 
-    public Player(int health){
+    public Player(int health)
+    {
         this.Health = health;
         AttackDamage = 50;
         MagicDamage = 100;
+        Mana = 500;
         PlayerClass = new Mage();
     }
 
-    public void TakeDamage(int damage){
+    public void TakeDamage(int damage)
+    {
         Health = Health - damage;
     }
 
