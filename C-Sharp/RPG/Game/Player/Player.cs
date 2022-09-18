51ac250adfe8de1f11c151a057c035daf13e66af
Shrimp <lcamara0916@gmail.com>
2022-09-18 23:06:17 +0000

    Added max values for health and mana

--- C-Sharp/RPG/Game/Player/Player.cs
+++ C-Sharp/RPG/Game/Player/Player.cs
@@ -6,8 +6,10 @@ public class Player
 
     public IClass PlayerClass { get; set; } = null!;
     public int Health { get; set; }
+    public int MaxHealth { get; set; }
     public int Resistance { get; set; }
     public int Mana { get; set; }
+    public int MaxMana { get; set; }
     public int AttackDamage { get; set; }
     public int MagicDamage { get; set; }
     public List<IItems> Inventory { get; set; }
@@ -16,8 +18,10 @@ public class Player
     public Player()
     {
         Health = 1000;
+        MaxHealth = 1000;
         Resistance = 0;
         Mana = 500;
+        MaxMana = 500;
         AttackDamage = 50;
         MagicDamage = 0;
         Inventory = new List<IItems>();
