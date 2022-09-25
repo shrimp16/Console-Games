bb40d8c39b2de264fe489efaef41c06224fc3e65
Shrimp <lcamara0916@gmail.com>
2022-09-25 14:50:35 +0000

    Added stock and stock list arrays on the potion shop

--- C-Sharp/RPG/Game/NPCS/Shops/PotionShop.cs
+++ C-Sharp/RPG/Game/NPCS/Shops/PotionShop.cs
@@ -4,9 +4,26 @@ namespace Shops;
 
 public class PotionShop : INPC
 {
+    public IItems[] stock = new IItems[]{
+            new SmallHealthPotion(),
+            new HealthPotion(),
+            new BigHealthPotion(),
+            new SmallManaPotion(),
+            new ManaPotion(),
+            new BigManaPotion()
+        };
+    public string[] stock_list = new string[]{
+        "Small Health Potion",
+        "Health Potion",
+        "Big Health Potion",
+        "Small Mana Potion",
+        "Mana Potion",
+        "Big Mana Potion"
+    };
+
     public void Interact(Player player)
     {
-        Menu menu = new Menu(new string[]{"lmao", "xd", "lol", "bruh"});
+        Menu menu = new Menu(stock_list);
         menu.Show();
         /*for(int i = 0; i < player.Inventory.Length; i++){
             if(player.Inventory[i] == null){
