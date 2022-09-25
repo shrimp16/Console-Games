17c7d2f5fe0fa35fe6df2a8a0116fa69cdcca3ca
Shrimp <lcamara0916@gmail.com>
2022-09-25 14:45:26 +0000

    Implemented a show method on the menu

--- C-Sharp/RPG/Game/Menu/Menu.cs
+++ C-Sharp/RPG/Game/Menu/Menu.cs
@@ -1,8 +1,24 @@
 class Menu
 {
     public string[] _content { get; set; }
+    public int pointer { get; set; }
     public Menu(string[] content)
     {
         _content = content;
+        pointer = 0;
+    }
+
+    public void Show()
+    {
+        Console.Clear();
+        for (int i = 0; i < _content.Length; i++)
+        {
+            if (i == pointer)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            Console.WriteLine(_content[i]);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
     }
 }
--- C-Sharp/RPG/Game/NPCS/Shops/PotionShop.cs
+++ C-Sharp/RPG/Game/NPCS/Shops/PotionShop.cs
@@ -6,11 +6,13 @@ public class PotionShop : INPC
 {
     public void Interact(Player player)
     {
-        for(int i = 0; i < player.Inventory.Length; i++){
+        Menu menu = new Menu(new string[]{"lmao", "xd", "lol", "bruh"});
+        menu.Show();
+        /*for(int i = 0; i < player.Inventory.Length; i++){
             if(player.Inventory[i] == null){
                 player.Inventory[i] = new HealthPotion();
                 return;
             }
-        }
+        }*/
     }
 }
