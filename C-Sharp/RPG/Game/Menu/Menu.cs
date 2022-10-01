3a52ecdbfb9c7ebd393cc19147135cba0d236cc6
Shrimp <lcamara0916@gmail.com>
2022-10-01 20:06:34 +0000

    Implemented the methods for the menu

--- C-Sharp/RPG/Game/Menu/Menu.cs
+++ C-Sharp/RPG/Game/Menu/Menu.cs
@@ -25,9 +25,35 @@ class Menu
 
     public int GetOption()
     {
+        while (selected == false)
+        {
+            KeyPress(Console.ReadKey(true).Key);
+        }
+        return pointer;
     }
 
     public void KeyPress(ConsoleKey key)
     {
+        switch (key)
+        {
+            case ConsoleKey.UpArrow:
+                if (pointer - 1 < 0)
+                {
+                    return;
+                }
+                pointer--;
+                break;
+            case ConsoleKey.DownArrow:
+                if (pointer + 1 >= _content.Length)
+                {
+                    return;
+                }
+                pointer++;
+                break;
+            case ConsoleKey.Enter:
+                selected = true;
+                break;
+        }
+        Show();
     }
 }
