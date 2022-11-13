b553fe40a69107b0b1e64e4744c92e72e65156ee
shrimp16 <lcamara0916@gmail.com>
2022-11-13 19:15:22 -0100

    Add first screen

--- C-Sharp/Rock Paper Scissors/Program.cs
+++ C-Sharp/Rock Paper Scissors/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program {
+    static public void Main(String[]args){
+        Game game = new Game();
+        game.Start();
+    }
+}
+
+public class Game {
+    public void Start(){
+        Console.Clear();
+        Console.WriteLine("Welcome to Console Rock Paper Scissors!");
+        Console.WriteLine("Press any key to start...");
+        Console.ReadKey(true);
+    }
+}
